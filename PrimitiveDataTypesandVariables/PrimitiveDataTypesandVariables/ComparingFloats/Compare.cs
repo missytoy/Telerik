7ed3e0d2116dac78f7ddfdd159e74312cd6fa7f0
missytoy <missytoy@gmail.com>
2015-01-21 22:59:49 +0200

    Primitive Data Types and Variables

--- PrimitiveDataTypesandVariables/PrimitiveDataTypesandVariables/ComparingFloats/Compare.cs
+++ PrimitiveDataTypesandVariables/PrimitiveDataTypesandVariables/ComparingFloats/Compare.cs
@@ -4,17 +4,34 @@
 //Note: Two floating-point numbers a and b cannot be compared directly by a == b because of the nature of the floating-point arithmetic. Therefore, we assume two numbers are equal if they are more closely to each other than a fixed constant eps.
 
 using System;
+using System.Threading;
+using System.Globalization;
+
 class Compare
 {
     static void Main()
     {
         {
+            //To ensure the decimal separator is "." and not ",". 
+            //Addin and "using System.Threading;" and " using System.Globalization; "
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
             double firstNumber, secondNumber;
-            Console.Write("Enter first real number  (with \",\" not \".\") :");
-            firstNumber = double.Parse(Console.ReadLine());
-            Console.Write("Enter second real number  (with \",\" not \".\") :");
-            secondNumber = Convert.ToDouble(Console.ReadLine());
-            bool isEqual = (firstNumber - secondNumber) < 1e-6;
+            string firstString = null, secondString = null;
+
+            Console.Write("Enter first real number :");
+            firstString = Console.ReadLine();
+            firstString = firstString.Replace(",", ".");
+            firstNumber = Double.Parse(firstString);
+
+            Console.Write("Enter second real number :");
+            secondString = Console.ReadLine();
+            secondString = secondString.Replace(",", ".");
+            secondNumber = Double.Parse(secondString);
+
+            double menos = firstNumber - secondNumber;
+            //To ensure that the number is positive we use Math.abs()
+            bool isEqual = Math.Abs(menos) < 0.000001;
             Console.WriteLine("Are these numbers equal? {0}", isEqual);
         }
     }
