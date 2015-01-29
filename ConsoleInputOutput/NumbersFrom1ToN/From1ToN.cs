99c111cf921f1603e4c8280b17d3aee48da8d8b6
missytoy <missytoy@gmail.com>
2015-01-29 02:12:51 +0200

    add

---
+++ ConsoleInputOutput/NumbersFrom1ToN/From1ToN.cs
@@ -0,0 +1,24 @@
+﻿//Problem 8. Numbers from 1 to n
+
+//Write a program that reads an integer number n from the console 
+//and prints all the numbers in the interval [1..n], each on a single line.
+
+using System;
+class From1ToN
+{
+    static void Main()
+    {
+        Console.Write("Enter end of interval (integer) n= ");
+        int n = int.Parse(Console.ReadLine());
+        while (n < 0 || n > 2147483647)
+        {
+            Console.Write("Enter end of interval (integer) n= ");
+            n = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ ConsoleInputOutput/PrintCompanyInformation/CompanyInformation.cs
@@ -0,0 +1,41 @@
+﻿//Problem 2. Print Company Information
+
+//A company has name, address, phone number, fax number, web site and manager. The manager has first name, last name, age and a phone number.
+//Write a program that reads the information about a company and its manager and prints it back on the console.
+
+using System;
+class CompanyInformation
+{
+    static void Main()
+    {
+        Console.Write("Company name:");
+        string companyName = Console.ReadLine();
+        Console.Write("Company address:");
+        string companyAdress = Console.ReadLine();
+        Console.Write("Phone number (without spaces):");
+        long phoneNumber = long.Parse(Console.ReadLine());
+        Console.Write("Fax number(without spaces):");
+        long faxNumber = Int64.Parse(Console.ReadLine());
+        Console.Write("Web site:");
+        string webSite = Console.ReadLine();
+        Console.Write("Manager first name:");
+        string firstName = Console.ReadLine();
+        Console.Write("Manager last name:");
+        string lastName = Console.ReadLine();
+        Console.Write("Manager age:");
+        int age= Convert.ToInt32(Console.ReadLine());
+        Console.Write("Manager phone:(without spaces):");
+        long phoneManager = long.Parse(Console.ReadLine());
+        Console.WriteLine(@"
+                            Company name: {0,}
+                            Company address: {1} 
+                            Phone number: {2} 
+                            Fax number: {3} 
+                            Web site: {4} 
+                            Manager first name: {5} 
+                            Manager last name: {6} 
+                            Manager age: {7} 
+                            Manager phone: {8}",
+        companyName,companyAdress,phoneNumber,faxNumber,webSite,firstName,lastName,age,phoneManager );
+    }
+}
